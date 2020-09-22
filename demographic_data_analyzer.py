3a7a38277bd0708e5807fc3932d9a6e42739cbd7
Ibrahim AMADOU <amadou68@gmail.com>
2020-09-22 12:13:49 +0000

    update percentage remain only three

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -1,11 +1,23 @@
 import pandas as pd
 
+
+def get_race_count(df):
+    # Begin here 
+    race_array = df['race'].tolist()
+    race_wo_dup_array = df['race'].drop_duplicates().tolist()
+    race_dict = dict() 
+    for item in race_wo_dup_array:
+        race_dict[item] = race_array.count(item)
+    
+    # return pd.DataFrame(race_dict.items(), columns=['race', 'race_count'])
+    return pd.Series(race_dict)
+    
 def calculate_demographic_data(print_data=True):
     # Read data from file
     df = pd.read_csv('adult_data.csv', sep=',')
     
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = df['race'].drop_duplicates()
+    race_count = get_race_count(df)
 
     # What is the average age of men?
     average_age_men = round(df[df['sex']=='Male']['age'].mean(), 1)
@@ -20,30 +32,35 @@ def calculate_demographic_data(print_data=True):
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
 
-    #print("Set: {}" .format(df[df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]))
-    high_salary_df = df[df['salary'] == '>50K']
+    higher_education = df[df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]
+    lower_education = df[~df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])] 
 
-    higher_education = (high_salary_df[high_salary_df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]['age'].count())/df['age'].count()
-    # print("higher {}".format(higher_education))
-
-    lower_education = (high_salary_df[~high_salary_df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]['age'].count())/df['age'].count()
-    # print("Lower {}".format(lower_education))
-
-
+    high_salary_df = higher_education[higher_education['salary'] == '>50K']
+    lower_education_high_salary_df  = lower_education[lower_education['salary'] == '>50K']
+  
     # percentage with salary >50K
-    higher_education_rich = higher_education * 100
-    lower_education_rich = lower_education * 100
+    percent_higher = high_salary_df['age'].count()/higher_education['age'].count()
+    percent_lower = lower_education_high_salary_df['age'].count()/lower_education['age'].count()
+
+    higher_education_rich = round(percent_higher * 100, 1)
+    lower_education_rich = round(percent_lower * 100, 1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
     min_work_hours = df['hours-per-week'].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = df[(df['hours-per-week'] == min_work_hours) & (df['salary'] == '>50K')]['age'].count()/df['age'].count()
+    num_min_workers_df = df[df['hours-per-week'] == min_work_hours]
+    
+    min_workers_rich_df = num_min_workers_df[num_min_workers_df['salary'] == '>50K']
 
-    rich_percentage = num_min_workers * 100
+    rich_percentage = (min_workers_rich_df['age'].count()/num_min_workers_df['age'].count()) * 100
+
 
     # What country has the highest percentage of people that earn >50K?
     high_salary_df = df[df['salary'] == '>50K']
+    high_salary_country = high_salary_df['native-country'].tolist()
+    country = max(high_salary_country, key=high_salary_country.count)    
+    print("Country: {}".format(high_salary_country))
     highest_earning_country = None
     highest_earning_country_percentage = None
 
